c917de0b40dba287d6db1c6c87c13c43be6c2128
Jared <jradtkey@gmail.com>
2017-08-21 13:39:43 -0700

    jared-edited

--- if_you_dont_mind.js
+++ if_you_dont_mind.js
@@ -4,10 +4,12 @@ function gimmeTheTime(hour, minute, period) {
   var timeOfDay = "";
   var minutes = 0;
 
-  if (period === "PM") {
-    timeOfDay = "evening"
-  } else {
-    timeOfDay = "morning"
+  if (period === "PM" & hour === 12 || hour <= 5 ) {
+    timeOfDay = "afternoon";
+  } else if (period === "PM" & hour > 17) {
+    timeOfDay = "evening";
+  } else if (period === "AM" & hour%12 < 12) {
+    timeOfDay = "morning";
   }
 
   if (minute === 15) {
@@ -22,19 +24,19 @@ function gimmeTheTime(hour, minute, period) {
   if (minute < 31 && period === "AM" && minute !== 30 && minute !== 15) {
     console.log("It's just after " + hour +  " in the " + timeOfDay);
   } else if (minute > 30 && period === "AM" && minute !== 45) {
-    console.log("It's almost " + nexthour + " in the " + timeOfDay);
+    console.log("It's almost " + nexthour%12 + " in the " + timeOfDay);
   } else if (minute < 31 && period === "PM" && minute !== 30 && minute !== 15) {
     console.log("It's just after " + hour + " in the " + timeOfDay);
   } else if (minute > 30 && period === "PM" && minute !== 45) {
-    console.log("It's almost " + nexthour + " in the " + timeOfDay);
+    console.log("It's almost " + nexthour%12 + " in the " + timeOfDay);
   } else if (minute === 30) {
-    console.log("It's " + minutes " past " + hour + " " + period);
+    console.log("It's " + minutes + " past " + hour + " " + timeOfDay);
   } else if (minute === 45) {
-    console.log("It's a quarter to " + nexthour + " " + period);
+    console.log("It's a quarter to " + nexthour%12 + " " + timeOfDay);
   } else if (minute === 15) {
-    console.log("It's a " + minutes + " past " + hour + " " + period);
+    console.log("It's a " + minutes + " past " + hour + " " + timeOfDay);
   }
 
 }
 
-gimmeTheTime(8, 30, "AM");
+gimmeTheTime(12, 36, "AM");
