b3d9a09cc2d15e794aaa9e508a67f0764c13c351
McCabe <mccabe@wambl.com>
2017-08-21 12:46:15 -0700

    updated my code

--- ifyoudontmind.js
+++ ifyoudontmind.js
@@ -1,21 +1,30 @@
-function ifYouDontMind(HOUR,MINUTE,PERIOD){
-  var HOUR = 8;
-  var MINUTE = 50;
-  var PERIOD = "AM";
+function ifYouDontMind(HOUR, MINUTE, PERIOD) {
+
 
   var TIMEOFDAY = '';
 
-   if(PERIOD == "AM"){
-     TIMEOFDAY = 'morning';
-   }
-   else {
-     TIMEOFDAY = 'evening';
-   }
-
-  if (MINUTE > 30){
-    console.log("it's almost " + (HOUR + 1) + " in the " + TIMEOFDAY);
+  if (PERIOD == "AM") {
+    TIMEOFDAY = 'morning';
+  } else if (PERIOD == "PM") {
+    TIMEOFDAY = 'evening';
+  } else {
+    TIMEOFDAY == 'undefined'
   }
-  else {
-    console.log("it's just after " + HOUR+ "in the " + TIMEOFDAY);
+
+  if (MINUTE > 30)
+
+
+
+  {
+    console.log("it's almost " + (HOUR + 1) + " in the " + TIMEOFDAY);
+  } else if (MINUTE = 30) {
+    console.log("it's half past " + (HOUR) + " in the " + TIMEOFDAY);
+  } else if (MINUTE = 15) {
+    console.log("it's quarter past " + (HOUR) + " in the " + TIMEOFDAY);
+  } else if (MINUTE = 45) {
+    console.log("it's quarter until " + (HOUR + 1) + " in the " + TIMEOFDAY);
+
+  } else {
+    console.log("it's just after " + HOUR + "in the " + TIMEOFDAY);
   }
 }
